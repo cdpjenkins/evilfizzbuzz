37e2468b463eeaee4ba86d57a79cd0b73d665308
Jason Gorman <jason.gorman@codemanship.com>
2019-09-09 12:28:31 +0100

    Generates 1..100

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -3,11 +3,20 @@ import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class FizzBuzz {
-    public List<Integer> generateListOfNumbers() {
+    public List<Integer> generateListOfNumbers(int upper) {
 
-      return IntStream.rangeClosed(1,100)
+      return IntStream.rangeClosed(1,upper)
               .mapToObj(Integer::new)
               .collect(Collectors.toList());
+    }
 
+    public String generateListOf10Numbers(int upper) {
+
+        return IntStream.rangeClosed(1,upper)
+                .mapToObj(Integer::toString)
+                .collect(Collectors.joining(","));
+
+
+        //return "1,2,3,4,5,6,7,8,9,10";
     }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -7,8 +7,15 @@ public class FizzBuzzTest {
 
     @Test
     public void shouldGenerate1to100(){
-        List<Integer> integerList = new FizzBuzz().generateListOfNumbers();
+        List<Integer> integerList = new FizzBuzz().generateListOfNumbers(100);
         Assert.assertEquals(100, integerList.size());
+    }
+
+    @Test
+    public void isOutputACommaDelimitedList()
+    {
+        String outputStringof10 = "1,2,3,4,5,6,7,8,9,10";
+        Assert.assertEquals(outputStringof10, new FizzBuzz().generateListOf10Numbers(10));
     }
 
 }

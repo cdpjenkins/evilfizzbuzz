1aac97f671a1170f6a45959208ea9a966c1a5006
Jason Gorman <jason.gorman@codemanship.com>
2019-09-09 12:09:06 +0100

    Generates 1..100

--- src/main/java/FizzBuzz.java
+++ src/main/java/FizzBuzz.java
@@ -1,2 +1,13 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class FizzBuzz {
+    public List<Integer> generateListOfNumbers() {
+
+      return IntStream.rangeClosed(1,100)
+              .mapToObj(Integer::new)
+              .collect(Collectors.toList());
+
+    }
 }
--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,2 +1,14 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
 public class FizzBuzzTest {
+
+    @Test
+    public void shouldGenerate1to100(){
+        List<Integer> integerList = new FizzBuzz().generateListOfNumbers();
+        Assert.assertEquals(100, integerList.size());
+    }
+
 }
